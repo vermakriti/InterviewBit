b2390751dfe236ccf71f6b16ea5b5bf29fae02de
vermakriti <kriti.verma.it@gmail.com>
2021-07-25 16:28:56 +0530

    Linked List

--- Linked List/LinkedList.cpp
+++ Linked List/LinkedList.cpp
@@ -351,3 +351,35 @@ int Solution::lPalin(ListNode* A) {
         return 1;
     return 0;     
 }
+
+//**********Kth Node From Middle*********//
+
+int Solution::solve(ListNode* A, int B) {
+    
+    ListNode*slow=A,*fast=A,*middle;
+    // finding middle of the list
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    middle=slow;
+    fast=A; slow=A;
+    bool flag=true;
+    int len=0;
+    while(fast->next!=middle){
+        len++;
+        if(len==B){
+            flag=false;
+            break;
+        }
+        fast=fast->next;
+    }
+    if(flag)return -1;
+    while(fast->next!=middle){
+        slow=slow->next;
+        fast=fast->next;
+    }
+    return slow->val;
+}
+
+//***********Even Reverse*********//
